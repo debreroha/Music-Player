d4af691df3bc73d47ac144fbb35e02fd3aba4715
Getnet Alemye <debreroha12@gmail.com>
2024-02-21 19:59:10 +0300

    finishing add song component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useSelector, useDispatch } from "react-redux";
-import { getSongsFetch, addSongFetch  } from './songState/songsState';
-import { useEffect } from "react";
-import { rootState } from './songState/songsState';
+import styled from '@emotion/styled';
 import GetSong from './components/GetSong';
 import AddSong from './components/AddSong';
 
+const Container = styled.div`
+  display: flex;
+  padding-top: 10px;
+  margin-left:200px;
+  margin-right:200px;
+  justify-content: center;
+  align-items: flex-start;
+  border: 1px solid black;
+  // width: 50px;
+  height: 100vh;
+`;
+
 const App = () => {
   return (
-    <>
+    <Container>
       <GetSong />
+      <br />
       <AddSong />
-
-    </>
+    </Container>
   );
 };
 
