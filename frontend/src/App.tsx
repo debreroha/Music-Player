04a5725c2bb76d7231724fec6941cc6622c21863
Getnet Alemye <debreroha12@gmail.com>
2024-02-23 19:09:21 +0300

    change some style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,7 @@ const Container = styled.div`
   background-color: #f2f2f2;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
   width: fit-content;
-  height: 300px; /* Set the desired height for the cards */
+  // height: 300px; 
 `;
 
 const App = () => {
